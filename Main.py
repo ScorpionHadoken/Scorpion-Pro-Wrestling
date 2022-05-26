d88688f87392d26f61df50077bf275fdf950896a
ScorpionHadoken <106120659+ScorpionHadoken@users.noreply.github.com>
2022-05-26 13:00:19 -0400

    updated Main.py
    Took everything out of the for loop
    
    Made the mouse button if statements nested into 1 if statement to speed it up a little more.
    
    Changed fps to 30 to ease the pain on my potato pc

--- Main.py
+++ Main.py
@@ -11,7 +11,7 @@ clock = pg.time.Clock()
 
 GameFont = pg.font.SysFont("C:\\Windows\Fonts\vgasys.fon", 35)
 
-def update_fps():
+def update_fps(): # Shows in the top left corner the current FPS
     fps = str(int(clock.get_fps()))
     fps_text = GameFont.render(fps, 1, pg.Color("red"))
     screen.blit(fps_text, (10, 10))
@@ -23,36 +23,36 @@ def main(self):
         pressed = pg.key.get_pressed()
         state = pg.mouse.get_pressed()
 
-        clock.tick(60)
+        clock.tick(30)
 
         for event in pg.event.get():
             
+            if event.type == pg.MOUSEBUTTONDOWN:
+                
+                if event.type == pg.MOUSEBUTTONDOWN and event.button == LEFT:
+                    Strike(self)
             
-            if pressed[pg.K_w]:
-                self.y -= self.spd
+                elif event.type == pg.MOUSEBUTTONDOWN and event.button == RIGHT:
+                    NotImplementedError() # This error tells the user that I'm lazy
+                
+                elif pressed[pg.K_SPACE]:
+                    Weak(self)
+
+        if pressed[pg.K_w]:
+            self.y -= self.spd
             
-            if pressed[pg.K_s]:
-                self.y += self.spd
-            
-            if pressed[pg.K_d]:
-                self.x += self.spd
-            
-            if pressed[pg.K_a]:
-                self.x -= self.spd
-            
-            screen.fill((200, 200, 200)) # I'll add wrestling ring l8r
-            
-            if event.type == pg.MOUSEBUTTONDOWN and event.button == LEFT:
-                Strike(self)
-            
-            elif event.type == pg.MOUSEBUTTONDOWN and event.button == RIGHT:
-                NotImplementedError() # This error tells the user that I'm lazy
-            
-            elif pressed[pg.K_SPACE]:
-                Weak(self)
-            
-            
-            screen.blit(self.img, (self.x, self.y))
+        if pressed[pg.K_s]:
+            self.y += self.spd
+        
+        if pressed[pg.K_d]:
+            self.x += self.spd
+        
+        if pressed[pg.K_a]:
+            self.x -= self.spd
+
+
+        screen.fill((200, 200, 200)) # I'll add wrestling ring l8r
+        screen.blit(self.img, (self.x, self.y))
         
         update_fps()        
         pg.display.flip()
